ee0932b9c584e571cbee78943961797a0277947d
Max Brener <linmaxi@gmail.com>
2025-01-20 21:56:16 +0200

    Integrated succesfully frontend and backend, added a 'serveHomePage' method

--- dev/Backend/src/main/java/groupId/Service.java
+++ dev/Backend/src/main/java/groupId/Service.java
@@ -8,8 +8,15 @@ import DTO.Records.Commands.ImageInputDTO;
 import DTO.Records.Image.ImageDTO;
 import DTO.Records.Image.SolutionDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 
 @RestController
 @RequestMapping("/")
@@ -20,22 +27,32 @@ public class Service implements ServiceInterface {
     public Service(UserController controller) {
         this.controller = controller;
     }
+
+    @GetMapping(value = {"/", "/{path:^(?!api|static).*$}/**"})
+    public ResponseEntity<Resource> serveHomePage() throws IOException {
+        Resource resource = new ClassPathResource("static/index.html");
+        return ResponseEntity.ok()
+            .contentType(MediaType.TEXT_HTML)
+            .body(resource);
+    }
+
     
     @PostMapping("/images")
-    public ResponseEntity<ImageDTO> createImage(@RequestBody CreateImageDTO zplFile) throws IOException {
+    public ResponseEntity<ImageDTO> createImage(@RequestBody CreateImageDTO zplFile) throws Exception {
         ImageDTO response = controller.createImage(zplFile);
         return ResponseEntity.ok(response);
     }
 
     @PatchMapping("/images")
-    public ResponseEntity<Void> configureImage(@RequestBody ImageConfigDTO imgConfig){
+    public ResponseEntity<Void> configureImage(@RequestBody ImageConfigDTO imgConfig) throws Exception{
         controller.configureImage(imgConfig);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/solve")
-    public ResponseEntity<SolutionDTO> solve(@RequestBody ImageInputDTO input) {
+    public ResponseEntity<SolutionDTO> solve(@RequestBody ImageInputDTO input) throws Exception {
         SolutionDTO res = controller.solve(input);
         return ResponseEntity.ok(res);
     }
+
 }
